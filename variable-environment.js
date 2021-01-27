43ac65b00e3b3f636e8a65b89d7525585f5415d9
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-01-26 22:27:23 -0300

    add variable environment example

---
+++ variable-environment.js
@@ -0,0 +1,16 @@
+function two() {
+    var isValid;
+}
+
+function one() {
+    var isValid = true;
+
+    two();
+}
+
+var isValid = false;
+one();
+
+// two() -- undefined
+// one() -- true
+// global -- false
