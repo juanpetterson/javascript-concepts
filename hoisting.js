14622248a2f8877bb5dea105b8beda66078ad6d0
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-01-26 22:16:55 -0300

    add hoisting example

---
+++ hoisting.js
@@ -0,0 +1,15 @@
+console.log(teddy);
+console.log(sign());
+
+var teddy = 'bear';
+
+// function expression
+var sign2 = function() {
+    console.log('uhhh la la la');
+}
+
+// function declaration
+function sign() {
+    console.log('ohhh la la la');
+}
+
