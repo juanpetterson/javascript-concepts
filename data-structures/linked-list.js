314471261780bdb2e8fa8d38ee21180db95fc9bd
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-02-21 19:47:14 -0300

    add insert and remove methods to linked lists

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -1,23 +1,15 @@
-// head -> body -> tail
-// 10 -> 5 -> 16
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 
-// let linkedList = {
-//     head: {
-//         value: 10,
-//         next: {
-//             value: 5,
-//             next: {
-//                 value: 16,
-//                 next: null
-//             }
-//         }
-//     }
-// }
 
+}
 class LinkedList {
     constructor(value) {
-        this.head = { 
-            value: value, 
+        this.head = {
+            value: value,
             next: null
         }
 
@@ -26,10 +18,7 @@ class LinkedList {
     }
 
     append(value) {
-        const newNode = {
-            value: value,
-            next: null
-        }
+        const newNode = new Node(value);
 
         this.tail.next = newNode;
         this.tail = newNode;
@@ -37,10 +26,89 @@ class LinkedList {
 
         return this;
     }
+
+    preppend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+
+        this.head = newNode;
+        this.length++;
+
+        return this;
+    }
+
+    insert(index, value) {
+        const newNode = new Node(value);
+
+        if(index >= this.length) {
+            return this.append(value);
+        }
+
+        const leaderNode = this.traverseToIndex(index - 1);
+
+        newNode.next = holdingPointer;
+        leaderNode.next = newNode;
+
+        this.length++;
+        return this;
+    }
+
+    remove(index) {
+        if(index >= this.length) {
+            throw new Error('Index out of bounds');
+        }
+
+        const leaderNode = this.traverseToIndex(index - 1);
+        const removeNode = leaderNode.next;
+        leaderNode.next = removeNode.next;
+
+        this.length--;
+        return this;
+    }
+
+    traverseToIndex(index) {
+        let count = 0;
+        let currentNode = this.head;
+
+        while(count < index) {
+            currentNode = currentNode.next;
+            count++;
+        }
+
+        return currentNode;
+    }
+
+    toArray() {
+        const array = [];
+        let currentNode = this.head;
+
+        while(currentNode){
+            array.push(currentNode.value);
+            
+            currentNode = currentNode.next;
+        }
+            
+
+        return array;
+    }
+
+    toString() {
+        return JSON.stringify(this);
+    }
+
+    // remove com posição OK
+    // insert com posição OK
+    // pop
+    // shift
 }
 
-const newLinkedList = new LinkedList(10);
-newLinkedList.append(5);
-newLinkedList.append(16);
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(15);
+myLinkedList.append(25);
+myLinkedList.preppend(0);
+myLinkedList.remove(2);
+myLinkedList.insert(4, 14);
 
-console.log(newLinkedList);
+console.log(myLinkedList.toString());
+console.log(myLinkedList.toArray());
